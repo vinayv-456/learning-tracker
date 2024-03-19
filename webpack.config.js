a54f22076bce1d7a18da5a56d3bd1c53b5e016b4
vinayv-456 <vinayvallabineni456@gmail.com>
2024-03-19 15:07:43 -0400

    webpack init with typescript config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+
+module.exports = {
+  // mode: "development", // 'production' or 'development'
+  entry: "./src/index.tsx",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"), // Path to serve static files from
+    },
+    open: true, // Open the default web browser
+    port: 8080, // Specify the port to listen on
+  },
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
